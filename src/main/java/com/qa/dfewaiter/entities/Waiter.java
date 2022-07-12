ba83b40d60f69c4a166fe460d4d22dc24216d1bc
jennifer.godman@udsl.com <jennifer.godman@udsl.com>
2022-07-12 21:08:21 +0100

    push public class DfewaiterApplication

---
+++ src/main/java/com/qa/dfewaiter/entities/Waiter.java
@@ -0,0 +1,5 @@
+package com.qa.dfewaiter.entities;
+
+public class Waiter {
+
+}
---
+++ src/main/java/com/qa/dfewaiter/services/WaiterService.java
@@ -0,0 +1,19 @@
+package com.qa.dfewaiter.services;
+
+import java.util.List;
+
+import com.qa.dfewaiter.entities.Waiter;
+
+public class WaiterService {
+
+	public List<Waiter> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Waiter readById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
