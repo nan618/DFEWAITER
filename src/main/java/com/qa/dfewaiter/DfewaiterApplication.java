d7ab1ae28cdbabe6ee8525223f2d53a1e2ca9954
jennifer.godman@udsl.com <jennifer.godman@udsl.com>
2022-07-12 11:38:42 +0100

    first commit

---
+++ src/main/java/com/qa/dfewaiter/DfewaiterApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dfewaiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfewaiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfewaiterApplication.class, args);
+	}
+
+}
